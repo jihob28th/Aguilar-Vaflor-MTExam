e846e4843d247bfa5f145f8d36a5cb0466369991
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-11 08:24:17 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,21 @@
+<html> 
+    <head> 
+        <title> World Sport Daily </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body> 
+        
+            <div class="backg"></div></br></br></br></br>
+                <div class="description"></br></br>
+                     DAILY
+             </br></br></br></br>
+             <img src="Images/logo01.png" style='height: 80px; width: 50%;' />
+                
+                <div class="about">
+                           test
+                </div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+            
+                           
+    </body>
+</html>
