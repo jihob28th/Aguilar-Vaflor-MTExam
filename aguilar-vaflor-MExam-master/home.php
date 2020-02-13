ab8f68e87a7a2a41e967b70d47087d85011401ba
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-13 10:41:04 +0800

    Add files via upload

---
+++ aguilar-vaflor-MExam-master/home.php
@@ -0,0 +1,18 @@
+<html> 
+    <head> 
+        <title> World Sport Daily </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body> 
+            <div class="backg01"></div></br></br></br></br>
+                <div class="desc"></br></br>
+             </br></br></br></br>
+             <img src="Images/logo01.png" style='height: 100px; width: 30%;' />
+                
+                <div class="about">
+                         "GET YOUR HEAD IN THE GAME!"
+                </div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+          
+    </body>
+</html>
